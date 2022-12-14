6221731a84c68f7aab9744971735bf4f3a568d44
bshlarissa <bslarissa1112@gmail.com>
2022-12-13 21:19:18 -0300

    first commit

---
+++ modulos.py
@@ -0,0 +1,19 @@
+quatro = 4
+dois = 2
+soma = quatro + dois
+print(soma)
+
+modulo = 10%3
+print(modulo)
+
+divisao_inteira = 15//2
+print(divisao_inteira)
+
+divisao = 15/2
+print(divisao)
+
+#comparacoes
+valor = 5 
+if valor == 5;
+print("show")
+valor1 = 3
